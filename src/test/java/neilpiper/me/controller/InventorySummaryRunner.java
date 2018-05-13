c98d898a9a7d327e7600bca97b0ad432c8a9e874
npiper <solveapuzzledev@gmail.com>
2018-05-13 04:27:43 +0100

    Unused import

--- src/test/java/neilpiper/me/controller/InventorySummaryRunner.java
+++ src/test/java/neilpiper/me/controller/InventorySummaryRunner.java
@@ -6,29 +6,26 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import com.intuit.karate.junit4.Karate;
-import cucumber.api.CucumberOptions;
 import neilpiper.me.MicroserviceReference;
 
 @RunWith(Karate.class)
 public class InventorySummaryRunner {
-  
+
   static ConfigurableApplicationContext appContext;
-  
+
   @BeforeClass
-  public static void startup()
-  {
+  public static void startup() {
     System.setProperty("server.port", "8090");
     appContext = SpringApplication.run(MicroserviceReference.class);
-    
-    
+
+
   }
-  
+
   @AfterClass
-  public static void tearDown()
-  {
+  public static void tearDown() {
     InventorySummaryRunner.appContext.stop();
   }
-  
+
 
 
 }
